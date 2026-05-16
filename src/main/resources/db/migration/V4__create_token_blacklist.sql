d8cd1c2118f347c29b51653db7ec4511eb2863fa
JestebanPedraza <pencan23@gmail.com>
2026-05-16 14:13:19 -0500

    fix: ajustar error

--- src/main/resources/db/migration/V4__create_token_blacklist.sql
+++ src/main/resources/db/migration/V4__create_token_blacklist.sql
@@ -3,5 +3,3 @@ CREATE TABLE token_blacklist (
     token VARCHAR(500) NOT NULL,
     expiry_date TIMESTAMP NOT NULL
 );
-    CONSTRAINT fk_usuarios_roles FOREIGN KEY (role_id) REFERENCES roles(id)
-);
