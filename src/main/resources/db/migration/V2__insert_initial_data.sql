3a894ff29b6d369abf4dc3620b71958eda28d978
JestebanPedraza <pencan23@gmail.com>
2026-05-16 13:47:33 -0500

    refactor: actualizar la contraseña del admin a la sugerida en los reqs

--- src/main/resources/db/migration/V2__insert_initial_data.sql
+++ src/main/resources/db/migration/V2__insert_initial_data.sql
@@ -4,4 +4,4 @@ INSERT INTO roles (nombre) VALUES ('GESTOR');
 
 -- Insertar Usuario Administrador
 INSERT INTO usuarios (nombre, email, password, role_id) 
-VALUES ('Administrador Sistema', 'admin@mail.com', '$2a$10$Xm5I16PZ5y.VozpYV6j.puxVv.h9WUn6D3.u8Q7gI9g9qWn.fIu7i', (SELECT id FROM roles WHERE nombre = 'ADMIN'));
+VALUES ('Administrador Sistema', 'admin@mail.com', '$2a$10$eeMB3FVSM2Oen8RIgXMwCud3lFL8sfcCedDTksqFr9pHhRzQ7p2ne', (SELECT id FROM roles WHERE nombre = 'ADMIN'));
